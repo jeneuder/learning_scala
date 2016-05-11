12b8c3e161587b98570d94edb356c5dd71e8f200
Joel Emanuel Neuder <jeneuder@gmail.com>
2016-05-10 19:00:14 -0600

    Adding a second line of text

--- src/scala_hello_world/HelloWorld.scala
+++ src/scala_hello_world/HelloWorld.scala
@@ -1,7 +1,10 @@
 
 
+package scala_hello_world
+
 object HelloWorld {
   def main(args: Array[String]): Unit = {
     println("Hello, world, again and again and again!")
+    println("A second line of text")
   }
 }
